84bfafae1ca563f6a8dc8cc9ed122b76ce282d13
FlorentCmb <florent.cmb77@gmail.com>
2020-01-13 10:20:10 +0100

    Clean React project

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Librairies
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Components & screens
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Styles
+import './index.css'
+
+// Others
+import * as serviceWorker from './serviceWorker'
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
+serviceWorker.register()
